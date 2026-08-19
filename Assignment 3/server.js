20725eff129709505e66e6d936e9874d71491468
Jaydeep Singh Chouhan <jschauhan7773@gmail.com>
2026-08-19 07:38:14 +0530

    Assessment 3

--- Assignment 3/server.js
+++ Assignment 3/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const app = express();
 const reviewRoutes = require("./src/routes/review.route");
 
-const app = express();
 app.use(express.json());
 
 app.use("/reviews", reviewRoutes);
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server:", error.message);
+  }
+};
+
+startServer(); 
 
 module.exports = app;
